7c3e35159242b4ff9d8b9933e7e4c6f2fea5dbc2
0603Adriano <0603adriano@gmail.com>
2021-07-27 11:29:47 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Calculadora de IMC
+
+Bem vindos ao meu projeto de calculadora de IMC!
+
+Neste projeto, desenvolvi um aplicativo para que  as pessoas consigam calcular o IMC e visualizar em qual faixa se encontram.
+
+O aplicativo conta com duas inserções dos usuário:
+
+- Peso (em kg)
+- Altura (em metros)
+
+E com essas informações, o IMC é calculado, e é exibido também, em qual faixa o usuário se encontra!
+
+## Screenshots
+
---
+++ app/src/main/java/com/adrianomartins/imccalc/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.adrianomartins.imccalc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        calcularBTN?.setOnClickListener {
+            calcularIMC(alturaEDT.text.toString(), pesoEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(altura: String, peso: String) {
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null) {
+            val imc = peso / (altura * altura)
+            titleTXT.text = "SEU IMC É: %.2f".format(imc)
+        }
+    }
+}
